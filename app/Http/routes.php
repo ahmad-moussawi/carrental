99542b7783982aa7da6e2cb94f45cf54da9e9786
Ahmad <ahmad.moussawi@gmail.com>
2015-05-31 13:30:31 +0300

    update readme

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+$app->group(['prefix' => 'admin'], function($app)
+{
+    $app->get('users', function()
+    {
+        // Matches The "/admin/users" URL
+    });
+});
+
 
 $app->get('/', function() use ($app) {
     return $app->welcome();
+});
+
+$app->get('/reservation', function() use ($app) {
+    return view('reservation');
 });
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Public website</title>
+    </head>
+    <body>
+       @yield('content')
+    </body>
+</html>
